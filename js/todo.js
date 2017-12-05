ea4e151d2072328fe7993d6a36ef81fa68af33eb
cgroark <colingroark@gmail.com>
2017-12-04 22:00:48 -0800

    complete sytling

--- js/todo.js
+++ js/todo.js
@@ -1,31 +1,12 @@
 
-// $("#inputButton").on("click", function(){	
-// 	var itemID = 
-// 	$("#listToDo").append($("<li>",{
-// 		text: $("#inputField").val()
-// 	}).append("<input type='checkbox' class='checkBox'/>"))
-// 	$("li").css("color", "red");
-// 	$("#inputField").val("");
-// 	$('.checkBox').on("click",function(){
-// 		console.log("demands!");
-// 		$("li")().css("color", "black");
+$("#inputButton").on("click", function(){	
+	$("#listToDo").append($("<li>",{
+		text: $("#inputField").val()
+	}))
+	$("li").css("color", "black").css("font-size", "22px").css("list-style-position", "inside").css("margin-bottom", "0px").css("margin-right", "30px");
+	$("#inputField").val("");
+	$("li").on("click",function(){
+		console.log("clicked");
+		this.remove();
 
-// });
-// })
-
-$("#inputButton").on("click", function(){
-var listItem = $("#inputField").val()	
-	$("#listToDo").append($("<li class ='items' id=" + listItem + ">" + listItem +"</li>",{
-}))
-$("li").css("color", "red");
-$("#inputField").val("");
-
-$(".items").on("click", function(){
-	console.log("clicked");
-	this.remove();
-	// $("listToDone").append("li");
-})
-
-
-})
-
+})});
