51570636583687dae011411f9f6a772084d2a0b4
cgroark <colingroark@gmail.com>
2017-12-05 08:58:11 -0800

    remove animation

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,18 @@
 
+var inputField = $("#inputField");
+var listToDo = $("#listToDo");
+var listToDone = $("#listToDone");
+
 $("#inputButton").on("click", function(){	
-	$("#listToDo").append($("<li>",{
-		text: $("#inputField").val()
+	listToDo.append($("<li>",{
+		text: inputField.val()
 	}))
 	$("li").css("color", "black").css("font-size", "22px").css("list-style-position", "inside").css("margin-bottom", "0px").css("margin-right", "30px");
 	$("#inputField").val("");
 	$("li").on("click",function(){
 		console.log("clicked");
-		this.remove();
-
+		// listToDone.appendTo($("<li>", function(){
+		// 	text: $(this).val()
+		// }))
+		$(this).css("text-decoration", "line-through").css("font-style", "italic").fadeOut(1500);
 })});
